8b9f0aa6d195b0f81384c38a69c8b409d9fa88b6
= <=>
2021-12-07 17:50:46 +0900

    TodoService와 TodoController 진행.
    RestController의 차이점과 스프링의 스테레오 타입들을 공부하자.

---
+++ src/main/java/com/starlab/withreact/todo/service/TodoService.java
@@ -0,0 +1,10 @@
+package com.starlab.withreact.todo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TodoService {
+    public String testService() {
+        return "Test Service";
+    }
+}
